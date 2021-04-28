78dbe7dcb1a1f04190dd27ca3b3ab836071dd3d8
FelixKerkmann <felix.kerkmann@students.fhnw.ch>
2021-04-28 11:57:05 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# README
+
+### Recommendation
+
+- Use a venv Environment
+
+### Requirements
+
+- install pip
+- tensorflow, numpy, matlibplot
---
+++ main.py
@@ -0,0 +1,16 @@
+import clothing
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    clothing.clothing()
+    
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
